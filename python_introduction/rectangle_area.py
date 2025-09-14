e837d8af4c65a48ee32ec4c900cc4cdda8c7dd30
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-09-14 13:57:24 -0400

    Add python_introduction tasks: basic operations, simple interest, rectangle area, hours->seconds, future age calculator

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+length = 10
+width = 5
+
+area = length * width
+
+print(f"The area of the rectangle is: {area}")
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
