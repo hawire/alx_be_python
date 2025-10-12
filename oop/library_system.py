9ca98c3bb15c6c19818f5cad773eacddfd671ce5
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-10-12 20:50:49 +0300

    Create library_system.py

---
+++ oop/library_system.py
@@ -0,0 +1,44 @@
+
+class Book:
+    def __init__(self, title, author):
+        """Initialize common attributes for all books."""
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        """Initialize EBook with file size and call parent constructor."""
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        """Initialize PrintBook with page count and call parent constructor."""
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        """Composition: Library contains a list of books."""
+        self.books = []
+
+    def add_book(self, book):
+        """Add a book to the library."""
+        self.books.append(book)
+
+    def list_books(self):
+        """List all books with details."""
+        for book in self.books:
+            print(book)
